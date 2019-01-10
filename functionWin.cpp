a792d51954175500eb7524b3384a4f4f64d0b206
TMwhuhan <1023280086@qq.com>
2019-01-10 09:28:54 +0800

    Create functionWin.cpp

---
+++ functionWin.cpp
@@ -0,0 +1,36 @@
+void DoProcessing(string& cmdline) 
+{
+  STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	//LPTSTR szCmdline = _tcsdup(TEXT("Driver\\demo\\Setup.exe"));
+	LPTSTR szCmdline = _strdup(TEXT(cmdline.c_str()));
+	// Start the child process. 
+	if (!CreateProcess(NULL,   // No module name (use command line)
+		szCmdline,        // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)           // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return;
+		//exit(0);
+	}
+
+	// Wait until child process exits.
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+}
